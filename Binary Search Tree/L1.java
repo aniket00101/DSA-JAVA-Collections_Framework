ec6fee97fe8373ec72529cab75b59afbf4ce74cc
aniket00101 <dasaniket971@gmail.com>
2026-01-15 23:36:13 +0530

    mini

---
+++ Binary Search Tree/L1.java
@@ -0,0 +1,15 @@
+/* 
+BST with a property
+1) every node to the left of given node is smaller than the node 
+2) every node to the right of given node is greater than the node
+
+Advantages:
+1) Efficient Searching. Time Complexity O(log n).
+2) Efficient insertion and deletion.
+3) Usage in implementation of other data structure like sets, map, priority queues etc.
+
+Disadvantage:
+1) Lack of support for range queries.
+2) Not that efficient in case of Binary Tree.
+
+*/
