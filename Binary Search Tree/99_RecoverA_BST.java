da9aa9b50dd44c2b3cbc514575e2e793c47db022
aniket00101 <dasaniket971@gmail.com>
2026-01-23 08:48:56 +0530

    add

--- Binary Search Tree/99_RecoverA_BST.java
+++ Binary Search Tree/99_RecoverA_BST.java
@@ -1,3 +1,24 @@
-public class 99_RecoverA_BST {
-    
+class Solution {
+    int idx = 0;
+
+    private void inorder(TreeNode root, List<Integer> res){
+        if(root == null)    return;
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
+    }
+
+    private void inorder2(TreeNode root, List<Integer> res){
+        if(root == null)    return;
+        inorder2(root.left, res);
+        root.val = res.get(idx++);
+        inorder2(root.right, res);
+    }
+
+    public void recoverTree(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(root, res);
+        Collections.sort(res);
+        inorder2(root, res);
+    }
 }
